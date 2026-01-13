23519d7ece8b35608ab249cf4912e3afccc02bd1
CBarnes99 <calumbarnes@hotmail.com>
2026-01-13 21:38:55 +0000

    Start Victory Screen
    CB - Started creating the victory screen

---
+++ Source/TowerDefense/HUDVictoryScreen.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "HUDVictoryScreen.generated.h"
+
+UCLASS()
+class TOWERDEFENSE_API UHUDVictoryScreen : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
